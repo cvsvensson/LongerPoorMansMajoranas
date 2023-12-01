66ece3025a195e8c2e3ddf83262f198dd9d570c0
Viktor Svensson <svensson.viktor.91@gmail.com>
2023-12-01 16:11:22 +0100

    .

--- src/optimize.jl
+++ src/optimize.jl
@@ -40,7 +40,7 @@ cost_reduced(reduced) = reduced^2
 
 best_algs() = [BBO_probabilistic_descent(), BBO_generating_set_search(), BBO_adaptive_de_rand_1_bin_radiuslimited(), Metaheuristics.SA(), Metaheuristics.DE(), Optim.IPNewton(), Optim.ConjugateGradient(), Optim.NelderMead()]
 best_alg_names() = string.([:BBO_probabilistic_descent, :BBO_generating_set_search, :BBO_adaptive_de_rand_1_bin_radiuslimited, :Metaheuristics_SA, :Metaheuristics_DE, :Optim_IPNewton, :Optim_ConjugateGradient, :Optim_NelderMead])
-function decompose(ps, N=div(2length(ps), 3))
+function decompose_rϕε(ps, N=div(2length(ps), 3))
     Nhalf = div(N + 1, 2)
     Nhalf2 = div(N, 2)
     rs = ps[1:Nhalf]
@@ -48,41 +48,90 @@ function decompose(ps, N=div(2length(ps), 3))
     εs = ps[Nhalf+Nhalf2+1:end]
     return rs, ϕs, εs
 end
-function get_hamfuncs(c, fixedparams)
+function decompose_ϕε(ps, N=length(ps))#div(length(ps), 2))
+    # Nhalf = div(N + 1, 2)
+    Nhalf2 = div(N, 2)
+    ϕs = ps[1:Nhalf2]
+    εs = ps[Nhalf2+1:end]
+    return ϕs, εs
+end
+splatter_rϕε(ps, N) = splatter_rϕε(decompose_rϕε(ps)..., N)
+function splatter_rϕε(rs, δϕs, ϵs, N)
+    # N = div(2 * (length(rs) + length(δϕs) + length(ϵs)), 3)
+    return vcat(reflect(rs, N) .* exp.(1im .* diffreflect(δϕs, N)), ϵs)
+end
+
+function hamfunc_rϕε(c, fixedparams)
     N = div(QuantumDots.nbr_of_fermions(c), 2)
     Nhalf = div(N + 1, 2)
     @variables Δ[1:N], εs[1:Nhalf]::Real
     params = merge(fixedparams, (; Δ=collect(Δ[1:N]), ε=reflect(εs, N)))
     f, f! = LongerPoorMansMajoranas.build_whamiltonian(c; params...)
-    fphase(Δs, εs) = f([Δs..., εs...])
-    fphase(rs, δϕs, ϵs) = fphase((reflect(rs, N) .* exp.(1im .* diffreflect(δϕs, N))), ϵs)
-    fphase(ps) = fphase(decompose(ps)...)
-    fphase!(out, Δs, εs) = f!(out, [Δs..., εs...])
-    fphase!(out, rs, δϕs, ϵs) = fphase!(out, (reflect(rs, N) .* exp.(1im .* diffreflect(δϕs, N))), ϵs)
-    fphase!(out, ps) = fphase!(out, decompose(ps)...)
+    f2(ps) = f(splatter_rϕε(ps, N))
+    f2!(out, ps) = f!(out, splatter_rϕε(ps, N))
     ps = rand(2Nhalf + div(N, 2))
-    cache = if c isa FermionBasis
-        blockdiagonal(fphase(ps), c)
-    elseif c isa FermionBdGBasis # Wait for update to QuantumDots
-        fphase(ps) |> Matrix |> BdGMatrix
-    end
-    return fphase, fphase!, cache
+    cache = get_cache(c, f2(ps))
+    return f2, f2!, cache
+end
+function hamfunc_ϕε(c, fixedparams)
+    N = div(QuantumDots.nbr_of_fermions(c), 2)
+    Nhalf = div(N + 1, 2)
+    Nhalf2 = div(N, 2)
+    @variables Δ[1:N], εs[1:Nhalf]::Real
+    params = merge(fixedparams, (; Δ=collect(Δ[1:N]), ε=reflect(εs, N)))
+    f, f! = LongerPoorMansMajoranas.build_whamiltonian(c; params...)
+    splatter_ϕε(ps, N) = splatter_ϕε(decompose_ϕε(ps)..., N)
+    splatter_ϕε(δϕs, ϵs, N) = vcat(fixedparams.Δ .* exp.(1im .* diffreflect(δϕs, N)), ϵs)
+    f2(ps) = f(splatter_ϕε(ps, N))
+    f2!(out, ps) = f!(out, splatter_ϕε(ps, N))
+    ps = rand(N)
+    cache = get_cache(c, f2(ps))
+    return f2, f2!, cache
 end
 
-@kwdef struct OptProb{B,P,F}
+
+
+get_cache(c::FermionBasis, ham) = blockdiagonal(ham, c)
+get_cache(c::FermionBdGBasis, ham) = BdGMatrix(Matrix(ham))
+# function get_hamfuncs(c, fixedparams, hamfunc)
+# N = div(QuantumDots.nbr_of_fermions(c), 2)
+# Nhalf = div(N + 1, 2)
+# @variables Δ[1:N], εs[1:Nhalf]::Real
+# params = merge(fixedparams, (; Δ=collect(Δ[1:N]), ε=reflect(εs, N)))
+# f, f! = LongerPoorMansMajoranas.build_whamiltonian(c; params...)
+# f2(ps) = f(splatter(ps))
+# f2!(out, ps) = f!(out, splatter(ps))
+# fphase(Δs, εs) = f([Δs..., εs...])
+# fphase(rs, δϕs, ϵs) = fphase((reflect(rs, N) .* exp.(1im .* diffreflect(δϕs, N))), ϵs)
+# fphase(ps) = fphase(decompose(ps)...)
+# fphase!(out, Δs, εs) = f!(out, [Δs..., εs...])
+# fphase!(out, rs, δϕs, ϵs) = fphase!(out, (reflect(rs, N) .* exp.(1im .* diffreflect(δϕs, N))), ϵs)
+# fphase!(out, ps) = fphase!(out, decompose(ps)...)
+# ps = rand(2Nhalf + div(N, 2))
+#     cache = if c isa FermionBasis
+#         blockdiagonal(f2(ps), c)
+#     elseif c isa FermionBdGBasis # Wait for update to QuantumDots
+#         f2(ps) |> Matrix |> BdGMatrix
+#     end
+#     return fphase, fphase!, cache
+# end
+
+@kwdef struct OptProb{F,B,P,T}
+    hamfunc::F
     basis::B
     fixedparams::P
-    target::F = x -> MPU(x) + LDf(x)
+    target::T = x -> MPU(x) + LDf(x)
+    minexcgap::Float64 = 0.0
 end
 
-opt_func(opt::OptProb, ::IPNewton) = opt_func_cons(opt.fixedparams, opt.basis, AutoFiniteDiff(), opt.target)
-opt_func(opt::OptProb, ::Optim.ConjugateGradient) = opt_func(opt.fixedparams, opt.basis, AutoFiniteDiff(), opt.target)
-opt_func(opt::OptProb, ::NelderMead) = opt_func(opt.fixedparams, opt.basis, AutoFiniteDiff(), opt.target)
-opt_func(opt::OptProb, alg) = opt_func(opt.fixedparams, opt.basis, nothing, opt.target)
+opt_func(opt::OptProb, ::IPNewton) = opt_func_cons(opt.hamfunc, opt.fixedparams, opt.basis, AutoFiniteDiff(), opt.target, opt.minexcgap)
+opt_func(opt::OptProb, ::Optim.ConjugateGradient) = opt_func(opt.hamfunc, opt.fixedparams, opt.basis, AutoFiniteDiff(), opt.target, opt.minexcgap)
+opt_func(opt::OptProb, ::NelderMead) = opt_func(opt.hamfunc, opt.fixedparams, opt.basis, AutoFiniteDiff(), opt.target, opt.minexcgap)
+opt_func(opt::OptProb, alg) = opt_func(opt.hamfunc, opt.fixedparams, opt.basis, nothing, opt.target, opt.minexcgap)
 
 
-function opt_func_cons(fixedparams, basis, ad=nothing, target=x -> MPU(x) + LDf(x))
-    f0, f!, cache = get_hamfuncs(basis, fixedparams)
+function opt_func_cons(hamfunc, fixedparams, basis, ad=nothing, target=x -> MPU(x) + LDf(x))
+    f0, f!, cache = hamfunc(basis, fixedparams)
     extra_cost(a, b) = 0.0
     fullsolve2(x) = fullsolve(f!(cache, x), basis)
     cost(x, p) = target(fullsolve2(x))
@@ -97,8 +146,8 @@ function opt_func_cons(fixedparams, basis, ad=nothing, target=x -> MPU(x) + LDf(
         return OptimizationFunction(cost, ad; cons), fullsolve2
     end
 end
-function opt_func(fixedparams, basis, ad=nothing, target=x -> MPU(x) + LDf(x))
-    f0, f!, cache = get_hamfuncs(basis, fixedparams)
+function opt_func(hamfunc, fixedparams, basis, ad=nothing, target=x -> MPU(x) + LDf(x))
+    f0, f!, cache = hamfunc(basis, fixedparams)
     # target = x -> MPU(x) + LDf(x)
     extra_cost(a, b) = 0.0
     f2(x) = fullsolve(f!(cache, x), basis)
@@ -115,23 +164,34 @@ function opt_func(fixedparams, basis, ad=nothing, target=x -> MPU(x) + LDf(x))
 end
 
 
-function get_initials(basis)
+function get_initials(hamfunc::typeof(hamfunc_rϕε), basis)
     N = div(length(basis), 2)
     [ones(div(N + 1, 2))..., pi .* ones(div(N, 2))..., zeros(div(N + 1, 2))...]
 end
-function get_ranges(basis)
+function get_initials(hamfunc::typeof(hamfunc_ϕε), basis)
+    N = div(length(basis), 2)
+    [pi .* ones(div(N, 2))..., zeros(div(N + 1, 2))...]
+end
+function get_ranges(hamfunc::typeof(hamfunc_ϕε), basis)
+    N = div(length(basis), 2)
+    Nhalf = div(N + 1, 2)
+    δϕranges = [(0.0, 2.0pi) for i in 1:div(N, 2)]
+    εranges = [(-20.0, 20.0) for i in 1:Nhalf]
+    [δϕranges..., εranges...]
+end
+function get_ranges(hamfunc::typeof(hamfunc_rϕε), basis)
     N = div(length(basis), 2)
     Nhalf = div(N + 1, 2)
     Δranges = [(0.1, 2.0) for i in 1:Nhalf]
     δϕranges = [(0.0, 2.0pi) for i in 1:div(N, 2)]
     εranges = [(-20.0, 20.0) for i in 1:Nhalf]
-    ranges = [Δranges..., δϕranges..., εranges...]
+    [Δranges..., δϕranges..., εranges...]
 end
-function SciMLBase.solve(prob::OptProb, alg; MaxTime=5, minexcgap=1 / 4, exps=collect(range(0.1, 3, length=4)), maxiters=1000, initials=get_initials(prob.basis), kwargs...)
+function SciMLBase.solve(prob::OptProb, alg; MaxTime=5, minexcgap=1 / 4, exps=collect(range(0.1, 3, length=4)), maxiters=1000, initials=get_initials(prob.hamfunc, prob.basis), kwargs...)
     f, fs = opt_func(prob, alg)
     refinements = length(exps)
     maxtime = MaxTime / refinements
-    ranges = get_ranges(prob.basis)
+    ranges = get_ranges(prob.hamfunc, prob.basis)
     lb = map(first, ranges)
     ub = map(last, ranges)
     newinitials = map(clamp, initials, lb, ub)
@@ -145,12 +205,12 @@ function SciMLBase.solve(prob::OptProb, alg; MaxTime=5, minexcgap=1 / 4, exps=co
         sol = solve(prob, alg; maxiters, maxtime, kwargs...)
     end
     optsol = fs(sol)
-    params = NamedTuple(zip((:Δ, :δϕ, :ε), decompose(sol)))
-    return (; alg, sol, optsol, params)
+    # params = NamedTuple(zip((:Δ, :δϕ, :ε), decompose(sol)))
+    return (; alg, sol, optsol)
 end
 
-function SciMLBase.solve(prob::OptProb, alg::IPNewton; MaxTime=5, minexcgap=1 / 4, maxiters=1000, initials=get_initials(prob.basis), kwargs...)
-    ranges = get_ranges(prob.basis)
+function SciMLBase.solve(prob::OptProb, alg::IPNewton; MaxTime=5, minexcgap=1 / 4, maxiters=1000, initials=get_initials(prob.hamfunc, prob.basis), kwargs...)
+    ranges = get_ranges(prob.hamfunc, prob.basis)
     lb = map(first, ranges)
     ub = map(last, ranges)
     println("Initial point: ", initials)
@@ -160,8 +220,8 @@ function SciMLBase.solve(prob::OptProb, alg::IPNewton; MaxTime=5, minexcgap=1 / 
     prob = OptimizationProblem(f, initials, minexcgap; lb, ub, lcons, ucons, kwargs...)#, allow_f_increases = true, store_trace = true)
     sol = solve(prob, alg; maxiters, maxtime=MaxTime, kwargs...)
     optsol = fs(sol)
-    params = NamedTuple(zip((:Δ, :δϕ, :ε), decompose(sol)))
-    return (; alg, sol, optsol, params)
+    # params = NamedTuple(zip((:Δ, :δϕ, :ε), decompose(sol)))
+    return (; alg, sol, optsol)
     # return sol
 end
 
