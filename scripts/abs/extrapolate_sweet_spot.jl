ed44d2ea2cbc36e4e445559dfa22d49cef586c1f
Viktor Svensson <svensson.viktor.91@gmail.com>
2024-01-12 16:15:49 +0100

    .

--- scripts/abs/extrapolate_sweet_spot.jl
+++ scripts/abs/extrapolate_sweet_spot.jl
@@ -21,7 +21,9 @@ function extrapolate_ss(sol, fixedparams, N)
     h = LongerPoorMansMajoranas.whamiltonian(basis; newparams...) |> hermitianpart! |> BdGMatrix
     fullsolve(h, basis)
 end
-
+##
+sols = Dict()
+bestsols = Dict()
 ## Find sweet spot in the N=2 case
 Δ0 = 1
 fixedparams = (; t=0.5, θ=parameter(2atan(5), :diff), V=0, Δ=Δ0, U=0.0)
@@ -29,42 +31,110 @@ N = 2
 basis = FermionBdGBasis(1:N, (:↑, :↓))
 optim = Rδϕ_Rε()
 alginds = [1, 1, 2, 3, 4]
-Ezs = collect(range(0.1, 8, length=50)) .* Δ0
-Ezsols = let
+Ezs = collect(range(0.1, 7, length=30)) .* Δ0
+let
     Ezsols = Vector{Any}(undef, length(Ezs))
     for (k, Ez) in collect(enumerate(Ezs))
         fp = merge(fixedparams, (; Ez))
         f, f!, cache = hamfunc(optim, basis, fp)
         prob = OptProb(; hamfunc=x -> f!(cache, x), basis, optparams=optim)
-        algsols = [solve(prob, alg; minexcgap=0.1, maxiters=1000000, MaxTime=2, exps=range(0.1, 3, 5)) for alg in best_algs()[alginds]]
+        algsols = [solve(prob, alg; minexcgap=0.1, maxiters=1000000, MaxTime=2, exps=range(0.1, 5, 5)) for alg in best_algs()[alginds]]
         Ezsols[k] = algsols
     end
-    Ezsols
+    sols[2] = Ezsols
 end;
-bestsols = map(x -> findmin(y -> MPU((y.optsol)), x)[2], Ezsols)
+##
+bestsols[2] = [sol[findmin(y -> MPU(y.optsol), sol)[2]] for sol in sols[2]]
 result = Dict("optparams" => map((s, n) -> collect(s[n].sol), Ezsols, bestsols), "Ez" => Ezs, "fixedparams" => fixedparams)
 ##
-# result2 = result
+p = plot(map(x -> x[1].optsol.gap, sols[2]))
+for k in Iterators.drop(eachindex(alginds), 1)
+    plot!(p, map(x -> x[k].optsol.gap, sols[2]))
+end
+p
+##
+p = plot(map(x -> MPU(x[1].optsol), sols[2]))
+for k in Iterators.drop(eachindex(alginds), 1)
+    plot!(p, map(x -> MPU(x[k].optsol), sols[2]))
+end
+p
+##
+extrapolatedsols = [extrapolate_ss(bestsols[2][k].sol, merge(fixedparams, (; Ez)), 3) for (k, Ez) in enumerate(Ezs)]
+##
 N = 3
 basis = FermionBdGBasis(1:N, (:↑, :↓))
-Ezs = result2["Ez"]
-Ezsols3 = let
+let
     Ezsols = Vector{Any}(undef, length(Ezs))
     for (k, Ez) in collect(enumerate(Ezs))
-        fp = merge(result2["fixedparams"], (; Ez))
+        sol = bestsols[2][k]
+        exsol = extrapolatedsols[k]
+        fp = merge(fixedparams, (; Ez))
         f, f!, cache = hamfunc(optim, basis, fp)
         prob = OptProb(; hamfunc=x -> f!(cache, x), basis, optparams=optim)
-        initials = extrapolate_ss_initials(result2["optparams"][k], N)
+        initials = extrapolate_ss_initials(sol.sol, N)
         println(initials)
-        algsols = [solve(prob, alg; initials, minexcgap=0.1, maxiters=1000000, MaxTime=2, exps=range(0.1, 3, 5)) for alg in best_algs()[alginds]]
+        algsols = [solve(prob, alg; initials, minexcgap=0.05, maxiters=1000000, MaxTime=10, exps=range(0.1, 5, 5)) for alg in best_algs()[alginds]]
         Ezsols[k] = algsols
     end
-    Ezsols
+    sols[3] = Ezsols
 end;
-bestsols3 = map(x -> findmin(y -> MPU((y.optsol)), x)[2], Ezsols3)
-result3 = Dict("optparams" => map((s, n) -> collect(s[n].sol), Ezsols3, bestsols3), "Ez" => Ezs, "fixedparams" => fixedparams)
+bestsols[3] = [sol[findmin(y -> MPU(y.optsol), sol)[2]] for sol in sols[3]]
+# result3 = Dict("optparams" => map((s, n) -> collect(s[n].sol), Ezsols3, bestsols3), "Ez" => Ezs, "fixedparams" => fixedparams)
+##
+
+##
+plot(map(x -> MPU(x.optsol), bestsols[2]))
+plot!(map(x -> MPU(x.optsol), bestsols[3]))
+plot!(map(MPU, extrapolatedsols))
+##
+plot(map(x -> LD(x.optsol), bestsols[2]))
+plot!(map(x -> LD(x.optsol), bestsols[3]))
+plot!(map(LD, extrapolatedsols))
+##
+plot(map(x -> x.optsol.excgap, bestsols[2]))
+plot!(map(x -> x.optsol.excgap, bestsols[3]))
+plot!(map(x -> x.excgap, extrapolatedsols))
+##
+plot(map(x -> x.optsol.gap, bestsols[2]))
+plot!(map(x -> x.optsol.gap, bestsols[3]))
+plot!(map(x -> x.gap, extrapolatedsols))
 ##
 map(MPU, result3)
+##
+Δ0 = 10
+fixedparams = (; t=0.5, θ=parameter(2atan(5), :diff), V=0, Δ=Δ0, U=0.0)
+f, f!, cache = hamfunc(optim, basis, merge(fixedparams, (; Ez=5 * Δ0)))
+##
+ε1 = range(4.88, 4.92, length=100) .* Δ0
+ε2 = range(4.85, 5, length=200) .* Δ0
+δϕ = range(pi/4, π/2, length=5)
+sols3d = [fullsolve(f!(cache, [δϕ, ε1, ε2]), basis) for (δϕ, ε1, ε2) in Iterators.product(δϕ, ε1, ε2)]
+##
+function slice_plot(sols, ε1=ε1, ε2=ε2)
+    cs = [:black, :white, :black]
+    ls = ([[-1e-2], [0.0], [1e-2]])
+    contours = map(x -> x.gap, sols |> permutedims)
+
+    z = map(log10 ∘ MPU, sols |> permutedims)
+    clims = (-2, 0.1)
+    p1 = heatmap(ε1, ε2, z; c=cgrad(:viridis, rev=true), clims, xlabel="ε1 and ε3", ylabel="ε2", title="log10(1-MPU)")
+    foreach((c, levels) -> contour!(p1, ε1, ε2, contours; c, levels), cs, ls)
+
+    z = map(log10 ∘ LD, sols |> permutedims)
+    clims = (-2, 0.1)
+    p2 = heatmap(ε1, ε2, z; c=cgrad(:viridis, rev=true), clims, xlabel="ε1 and ε3", ylabel="ε2", title="log10(LD)", cbar=false)
+    foreach((c, levels) -> contour!(p2, ε1, ε2, contours; c, levels), cs, ls)
+
+    z = map(x -> x.excgap, sols |> permutedims)
+    clims = (0, 0.3)
+    p3 = heatmap(ε1, ε2, z; c=cgrad(:viridis, rev=false), clims, xlabel="ε1 and ε3", ylabel="ε2", title="excgap")
+    foreach((c, levels) -> contour!(p2, ε1, ε2, contours; c, levels), cs, ls)
+    return plot(p2, p3; layout=(1, 2), size=0.7 .* (1200, 400))
+end
+mapslices(display ∘ slice_plot, sols3d, dims=[2, 3])
+##
+
+map(MPU, sols3d[1, :, :])
 
 ##
 wsave(sdatadir("Ezsols_small_N3.jld2"), result3)
@@ -92,10 +162,10 @@ plot(map(MPU, sols2))
 plot!(map(MPU, sols3))
 plot(map(LD, sols2))
 plot!(map(LD, sols3))
-plot(map(x->x.excgap, sols2))
-plot!(map(x->x.excgap, sols3))
-plot(map(x->x.gap, sols2))
-plot!(map(x->x.gap, sols3))
+plot(map(x -> x.excgap, sols2))
+plot!(map(x -> x.excgap, sols3))
+plot(map(x -> x.gap, sols2))
+plot!(map(x -> x.gap, sols3))
 
 ##
 
